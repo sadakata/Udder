b953e3aa653acb00b6189d73ee0040f3bc028345
Wataru Sadakata <sadakata@gmail.com>
2020-05-29 09:40:03 +0900

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by wat on 2020/05/29.
+//  Copyright © 2020 Wataru Sadakata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
